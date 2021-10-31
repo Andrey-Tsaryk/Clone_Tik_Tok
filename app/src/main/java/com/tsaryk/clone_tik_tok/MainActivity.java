dc136523354edf962b9dec6e7c19c720efca79de
Andrey Tsaryk <tsaryk1323an@gmail.com>
2021-10-31 19:20:40 +0300

    splash screen

--- app/src/main/java/com/tsaryk/clone_tik_tok/MainActivity.java
+++ app/src/main/java/com/tsaryk/clone_tik_tok/MainActivity.java
@@ -2,7 +2,10 @@ package com.tsaryk.clone_tik_tok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // status bar hide start
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
